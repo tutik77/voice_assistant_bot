07b069841adf7282817044b37e012519c7fa3938
tutik77 <tutikosnov@gmail.com>
2024-06-27 21:34:16 +0300

    юзаю асинхронный клиент

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ from config import settings
 import openai
 import asyncio
 import os
-from openai import OpenAI
+from openai import AsyncOpenAI
 from aiogram.types import FSInputFile
 
 logging.basicConfig(level=logging.INFO)
@@ -21,13 +21,11 @@ if not os.path.exists(DIRECTORY):
 
 os.environ['OPENAI_API_KEY'] = settings.openai_api_key
 openai.api_key = settings.openai_api_key
-client = OpenAI()
-
-
+client = AsyncOpenAI()
 
 async def convert_voice_to_text(local_path: str) -> str:
     with open(local_path, "rb") as audio_file:
-        response = client.audio.transcriptions.create(
+        response = await client.audio.transcriptions.create(
             model="whisper-1",
             file=audio_file
         )
@@ -36,7 +34,7 @@ async def convert_voice_to_text(local_path: str) -> str:
 @dp.message(CommandStart())
 async def whatsupp_bro(message: types.Message):
     global assistant
-    assistant = client.beta.assistants.create(
+    assistant = await client.beta.assistants.create(
         name="helpful assistant",
         instructions = "You are personal assistant",
         tools=[{"type": "code_interpreter"}],
@@ -44,7 +42,7 @@ async def whatsupp_bro(message: types.Message):
     )
 
     global thread
-    thread = client.beta.threads.create()
+    thread = await client.beta.threads.create()
     await message.reply("Бот запущен")
 
 @dp.message(F.voice)
@@ -56,20 +54,22 @@ async def handle_voice_message(message: types.Message):
     await bot.download_file(file_path, local_path)
     text = await convert_voice_to_text(local_path)
 
-    mess = client.beta.threads.messages.create(
+    mess = await client.beta.threads.messages.create(
         thread_id=thread.id,
         role="user",
         content=text
     )
 
-    run = client.beta.threads.runs.create_and_poll(
+    run = await client.beta.threads.runs.create_and_poll(
     thread_id=thread.id,
     assistant_id=assistant.id,
     )      
     if run.status == "completed":
-        messages = client.beta.threads.messages.list(thread_id=thread.id)
-        if messages.data and messages.data[0].content[0].type == "text":
-            response = messages.data[0].content[0].text.value
+        messages = await client.beta.threads.messages.list(thread_id=thread.id)
+        async for messagee in messages:
+            if messagee.content[0].type == "text":
+                response = messagee.content[0].text.value
+                break
 
     speech_file_path = f"{DIRECTORY}/speech.mp3"
     with openai.audio.speech.with_streaming_response.create(
