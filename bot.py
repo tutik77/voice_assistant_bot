c880ef18df1010b9120d12116ad80d6af7f3dc26
tutik77 <tutikosnov@gmail.com>
2024-06-27 14:52:32 +0300

    отвечает голосом, создаются разные ассистенты для пользователей

--- bot.py
+++ bot.py
@@ -7,10 +7,13 @@ import openai
 import asyncio
 import os
 from openai import OpenAI
+from aiogram.types import FSInputFile
+
 logging.basicConfig(level=logging.INFO)
 
 bot = Bot(token=settings.bot_token)
 dp = Dispatcher()
+
 DIRECTORY = "voices"
 if not os.path.exists(DIRECTORY):
     os.makedirs(DIRECTORY)
@@ -21,14 +24,6 @@ openai.api_key = settings.openai_api_key
 client = OpenAI()
 
 
-assistant = client.beta.assistants.create(
-    name="helpful assistant",
-    instructions = "You are personal assistant",
-    tools=[{"type": "code_interpreter"}],
-    model="gpt-4o",
-)
-
-thread = client.beta.threads.create()
 
 async def convert_voice_to_text(local_path: str) -> str:
     with open(local_path, "rb") as audio_file:
@@ -40,7 +35,17 @@ async def convert_voice_to_text(local_path: str) -> str:
 
 @dp.message(CommandStart())
 async def whatsupp_bro(message: types.Message):
-    await message.reply("Салам че")
+    global assistant
+    assistant = client.beta.assistants.create(
+        name="helpful assistant",
+        instructions = "You are personal assistant",
+        tools=[{"type": "code_interpreter"}],
+        model="gpt-4o",
+    )
+
+    global thread
+    thread = client.beta.threads.create()
+    await message.reply("Бот запущен")
 
 @dp.message(F.voice)
 async def handle_voice_message(message: types.Message):
@@ -51,7 +56,7 @@ async def handle_voice_message(message: types.Message):
     await bot.download_file(file_path, local_path)
     text = await convert_voice_to_text(local_path)
 
-    response = client.beta.threads.messages.create(
+    mess = client.beta.threads.messages.create(
         thread_id=thread.id,
         role="user",
         content=text
@@ -61,14 +66,21 @@ async def handle_voice_message(message: types.Message):
     thread_id=thread.id,
     assistant_id=assistant.id,
     )      
-    
     if run.status == "completed":
         messages = client.beta.threads.messages.list(thread_id=thread.id)
         if messages.data and messages.data[0].content[0].type == "text":
-            await bot.send_message(message.chat.id, messages.data[0].content[0].text.value)
+            response = messages.data[0].content[0].text.value
 
+    speech_file_path = f"{DIRECTORY}/speech.mp3"
+    with openai.audio.speech.with_streaming_response.create(
+        model="tts-1",
+        voice="alloy",
+        input=response,
+    ) as response_speech:
+        response_speech.stream_to_file(speech_file_path)
 
-
+    voice = FSInputFile(speech_file_path)
+    await bot.send_voice(message.chat.id, voice)
 
 async def main():
     await dp.start_polling(bot)
